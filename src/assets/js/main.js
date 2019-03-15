c165cc4c2ca170bcf799f3d51560e1c13fb06baf
Dat NGUYEN <datle@itc-admin-mm-015s-Mac-mini.local>
2019-03-15 18:55:27 +0700

    fx-mayr 50%

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     var images = document.getElementsByClassName('parallax');
     var instances = new simpleParallax(images, {
-        delay: 0.6,
-        orientation: 'up',
+        delay: 0.8,
+        orientation: 'down',
         scale: 1.2,
         overfow: false,
     });
