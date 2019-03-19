daaca129333b0226c5bd772e176dbe40beae9e37
Dat NGUYEN <datle@itc-admin-mm-015s-Mac-mini.local>
2019-03-19 18:56:20 +0700

    fix bug

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,11 +11,18 @@ $(document).ready(function () {
     $('#be-called').click(function () {
         $('.called-info, #optional-phone-number').toggle(); 
     });
+    // Header animation
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 240) {
+            $('.header').addClass('header--sticked');
+        } else {
+            $('.header').removeClass('header--sticked');
+        }
+    });
 });
 //trigger collapse people-items
 (function ($) {
     var $window = $(window)
-
     function resize() {
         if ($window.width() < 768) {
             return $('.collapseItem').addClass('collapse');
